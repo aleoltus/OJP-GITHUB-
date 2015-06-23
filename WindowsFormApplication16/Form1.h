e7a25fa197b96f57750dcd46ec73e4b07d6532da
aleoltus <gandiiify@gmail.com>
2015-06-23 12:30:34 +0100

    dodanie promili.. masy .. ilosci

--- WindowsFormApplication16/Form1.h
+++ WindowsFormApplication16/Form1.h
@@ -34,10 +34,15 @@ namespace WindowsFormApplication1 {
 				delete components;
 			}
 		}
-	private: System::Windows::Forms::CheckBox^  dod;
-	private: System::Windows::Forms::CheckBox^  ode;
-	private: System::Windows::Forms::CheckBox^  mno;
-	private: System::Windows::Forms::CheckBox^  dzie;
+	private: System::Windows::Forms::CheckBox^  piw;
+	protected:
+
+	private: System::Windows::Forms::CheckBox^  wino;
+	private: System::Windows::Forms::CheckBox^  wódka;
+	private: System::Windows::Forms::CheckBox^  inne;
+
+
+
 	protected:
 
 	protected:
@@ -51,7 +56,11 @@ namespace WindowsFormApplication1 {
 	private: System::Windows::Forms::TextBox^  licz2;
 	private: System::Windows::Forms::TextBox^  wyn;
 	private: System::Windows::Forms::Label^  symbol;
-	private: System::Windows::Forms::Button^  button1;
+	private: System::Windows::Forms::TextBox^  Masa;
+	private: System::Windows::Forms::TextBox^  Ilosc;
+
+
+
 
 
 
@@ -69,75 +78,77 @@ namespace WindowsFormApplication1 {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->dod = (gcnew System::Windows::Forms::CheckBox());
-			this->ode = (gcnew System::Windows::Forms::CheckBox());
-			this->mno = (gcnew System::Windows::Forms::CheckBox());
-			this->dzie = (gcnew System::Windows::Forms::CheckBox());
+			this->piw = (gcnew System::Windows::Forms::CheckBox());
+			this->wino = (gcnew System::Windows::Forms::CheckBox());
+			this->wódka = (gcnew System::Windows::Forms::CheckBox());
+			this->inne = (gcnew System::Windows::Forms::CheckBox());
 			this->label1 = (gcnew System::Windows::Forms::Label());
 			this->licz = (gcnew System::Windows::Forms::Button());
 			this->licz1 = (gcnew System::Windows::Forms::TextBox());
 			this->licz2 = (gcnew System::Windows::Forms::TextBox());
 			this->wyn = (gcnew System::Windows::Forms::TextBox());
 			this->symbol = (gcnew System::Windows::Forms::Label());
-			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->Masa = (gcnew System::Windows::Forms::TextBox());
+			this->Ilosc = (gcnew System::Windows::Forms::TextBox());
 			this->SuspendLayout();
 			// 
-			// dod
+			// piw
 			// 
-			this->dod->AutoSize = true;
-			this->dod->Location = System::Drawing::Point(527, 42);
-			this->dod->Name = L"dod";
-			this->dod->Size = System::Drawing::Size(78, 17);
-			this->dod->TabIndex = 0;
-			this->dod->Text = L"dodawanie";
-			this->dod->UseVisualStyleBackColor = true;
-			this->dod->CheckedChanged += gcnew System::EventHandler(this, &Form1::checkBox1_CheckedChanged);
+			this->piw->AutoSize = true;
+			this->piw->Location = System::Drawing::Point(257, 50);
+			this->piw->Name = L"piw";
+			this->piw->Size = System::Drawing::Size(48, 17);
+			this->piw->TabIndex = 0;
+			this->piw->Text = L"piwo";
+			this->piw->UseVisualStyleBackColor = true;
+			this->piw->CheckedChanged += gcnew System::EventHandler(this, &Form1::checkBox1_CheckedChanged);
 			// 
-			// ode
+			// wino
 			// 
-			this->ode->AutoSize = true;
-			this->ode->Location = System::Drawing::Point(527, 65);
-			this->ode->Name = L"ode";
-			this->ode->Size = System::Drawing::Size(88, 17);
-			this->ode->TabIndex = 1;
-			this->ode->Text = L"odejmowanie";
-			this->ode->UseVisualStyleBackColor = true;
-			this->ode->CheckedChanged += gcnew System::EventHandler(this, &Form1::ode_CheckedChanged);
+			this->wino->AutoSize = true;
+			this->wino->Location = System::Drawing::Point(257, 73);
+			this->wino->Name = L"wino";
+			this->wino->Size = System::Drawing::Size(48, 17);
+			this->wino->TabIndex = 1;
+			this->wino->Text = L"wino";
+			this->wino->UseVisualStyleBackColor = true;
+			this->wino->CheckedChanged += gcnew System::EventHandler(this, &Form1::ode_CheckedChanged);
 			// 
-			// mno
+			// wódka
 			// 
-			this->mno->AutoSize = true;
-			this->mno->Location = System::Drawing::Point(527, 88);
-			this->mno->Name = L"mno";
-			this->mno->Size = System::Drawing::Size(71, 17);
-			this->mno->TabIndex = 2;
-			this->mno->Text = L"mnozenie";
-			this->mno->UseVisualStyleBackColor = true;
-			this->mno->CheckedChanged += gcnew System::EventHandler(this, &Form1::mno_CheckedChanged);
+			this->wódka->AutoSize = true;
+			this->wódka->Location = System::Drawing::Point(257, 96);
+			this->wódka->Name = L"wódka";
+			this->wódka->Size = System::Drawing::Size(58, 17);
+			this->wódka->TabIndex = 2;
+			this->wódka->Text = L"wódka";
+			this->wódka->UseVisualStyleBackColor = true;
+			this->wódka->CheckedChanged += gcnew System::EventHandler(this, &Form1::mno_CheckedChanged);
 			// 
-			// dzie
+			// inne
 			// 
-			this->dzie->AutoSize = true;
-			this->dzie->Location = System::Drawing::Point(527, 111);
-			this->dzie->Name = L"dzie";
-			this->dzie->Size = System::Drawing::Size(67, 17);
-			this->dzie->TabIndex = 3;
-			this->dzie->Text = L"dzielenie";
-			this->dzie->UseVisualStyleBackColor = true;
-			this->dzie->CheckedChanged += gcnew System::EventHandler(this, &Form1::dzie_CheckedChanged);
+			this->inne->AutoSize = true;
+			this->inne->Location = System::Drawing::Point(257, 119);
+			this->inne->Name = L"inne";
+			this->inne->Size = System::Drawing::Size(46, 17);
+			this->inne->TabIndex = 3;
+			this->inne->Text = L"inne";
+			this->inne->UseVisualStyleBackColor = true;
+			this->inne->CheckedChanged += gcnew System::EventHandler(this, &Form1::dzie_CheckedChanged);
 			// 
 			// label1
 			// 
 			this->label1->AutoSize = true;
-			this->label1->Location = System::Drawing::Point(404, 127);
+			this->label1->Location = System::Drawing::Point(254, 171);
 			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(34, 13);
+			this->label1->Size = System::Drawing::Size(41, 13);
 			this->label1->TabIndex = 4;
-			this->label1->Text = L"wynik";
+			this->label1->Text = L"Promile";
+			this->label1->Click += gcnew System::EventHandler(this, &Form1::label1_Click);
 			// 
 			// licz
 			// 
-			this->licz->Location = System::Drawing::Point(523, 149);
+			this->licz->Location = System::Drawing::Point(122, 106);
 			this->licz->Name = L"licz";
 			this->licz->Size = System::Drawing::Size(75, 23);
 			this->licz->TabIndex = 5;
@@ -147,7 +158,7 @@ namespace WindowsFormApplication1 {
 			// 
 			// licz1
 			// 
-			this->licz1->Location = System::Drawing::Point(389, 39);
+			this->licz1->Location = System::Drawing::Point(12, 43);
 			this->licz1->Name = L"licz1";
 			this->licz1->Size = System::Drawing::Size(66, 20);
 			this->licz1->TabIndex = 6;
@@ -155,14 +166,14 @@ namespace WindowsFormApplication1 {
 			// 
 			// licz2
 			// 
-			this->licz2->Location = System::Drawing::Point(389, 85);
+			this->licz2->Location = System::Drawing::Point(12, 108);
 			this->licz2->Name = L"licz2";
 			this->licz2->Size = System::Drawing::Size(66, 20);
 			this->licz2->TabIndex = 7;
 			// 
 			// wyn
 			// 
-			this->wyn->Location = System::Drawing::Point(375, 149);
+			this->wyn->Location = System::Drawing::Point(228, 198);
 			this->wyn->Name = L"wyn";
 			this->wyn->Size = System::Drawing::Size(100, 20);
 			this->wyn->TabIndex = 8;
@@ -171,37 +182,47 @@ namespace WindowsFormApplication1 {
 			// symbol
 			// 
 			this->symbol->AutoSize = true;
-			this->symbol->Location = System::Drawing::Point(404, 62);
+			this->symbol->Location = System::Drawing::Point(139, 50);
 			this->symbol->Name = L"symbol";
 			this->symbol->Size = System::Drawing::Size(39, 13);
 			this->symbol->TabIndex = 9;
 			this->symbol->Text = L"symbol";
 			// 
-			// button1
+			// Masa
 			// 
-			this->button1->Location = System::Drawing::Point(375, 254);
-			this->button1->Name = L"button1";
-			this->button1->Size = System::Drawing::Size(107, 29);
-			this->button1->TabIndex = 10;
-			this->button1->Text = L"button1";
-			this->button1->UseVisualStyleBackColor = true;
+			this->Masa->Location = System::Drawing::Point(12, 17);
+			this->Masa->Name = L"Masa";
+			this->Masa->Size = System::Drawing::Size(100, 20);
+			this->Masa->TabIndex = 10;
+			this->Masa->Text = L"Masa";
+			this->Masa->TextChanged += gcnew System::EventHandler(this, &Form1::textBox1_TextChanged);
+			// 
+			// Ilosc
+			// 
+			this->Ilosc->Location = System::Drawing::Point(13, 82);
+			this->Ilosc->Name = L"Ilosc";
+			this->Ilosc->Size = System::Drawing::Size(100, 20);
+			this->Ilosc->TabIndex = 11;
+			this->Ilosc->Text = L"Iloœc ";
+			this->Ilosc->TextChanged += gcnew System::EventHandler(this, &Form1::textBox1_TextChanged_1);
 			// 
 			// Form1
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(617, 392);
-			this->Controls->Add(this->button1);
+			this->ClientSize = System::Drawing::Size(492, 240);
+			this->Controls->Add(this->Ilosc);
+			this->Controls->Add(this->Masa);
 			this->Controls->Add(this->symbol);
 			this->Controls->Add(this->wyn);
 			this->Controls->Add(this->licz2);
 			this->Controls->Add(this->licz1);
 			this->Controls->Add(this->licz);
 			this->Controls->Add(this->label1);
-			this->Controls->Add(this->dzie);
-			this->Controls->Add(this->mno);
-			this->Controls->Add(this->ode);
-			this->Controls->Add(this->dod);
+			this->Controls->Add(this->inne);
+			this->Controls->Add(this->wódka);
+			this->Controls->Add(this->wino);
+			this->Controls->Add(this->piw);
 			this->Name = L"Form1";
 			this->Text = L"Form1";
 			this->Load += gcnew System::EventHandler(this, &Form1::Form1_Load);
@@ -243,6 +264,10 @@ private: System::Void mno_CheckedChanged(System::Object^  sender, System::EventA
 }
 private: System::Void dzie_CheckedChanged(System::Object^  sender, System::EventArgs^  e) {
 	this->symbol->Text = "/";
+}
+private: System::Void button1_Click(System::Object^  sender, System::EventArgs^  e) {
+}
+private: System::Void label1_Click(System::Object^  sender, System::EventArgs^  e) {
 }
 };
 }
